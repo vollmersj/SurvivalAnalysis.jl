b8f293d50853bd3384a45bd42a973cad9c0a2dfc
RaphaelS1 <raphaelsonabend@gmail.com>
2022-07-02 10:34:11 +0100

    update tests

--- src/test.jl
+++ src/test.jl
@@ -5,56 +5,15 @@ using BenchmarkTools
 using Survival
 
 seed!(1)
-n = 100
+n = 1000
 T = round.(rand(Uniform(1, 10), n));
 Δ = rand(Binomial(), n) .== 1;
 ot = Surv(T, Δ, "right");
 Surv(T, Δ .== 1, "right")
 et = EventTime.(T, Δ);
 
-@btime confint.(Ref(kaplan(ot)), uniqueEventTimes(ot));
-@btime confint(fit(Survival.KaplanMeier, et));
+@benchmark confint.(Ref(kaplan(ot)), uniqueEventTimes(ot))
+@benchmark confint(fit(Survival.KaplanMeier, et))
 
-map(v -> map(x -> -log(x), v), confint.(Ref(nelson(ot)), uniqueEventTimes(ot)))
-confint(fit(Survival.NelsonAalen, et))
-
-@btime nelson(ot);
-@btime nelson2(ot);
-
-@assert kaplan(ot).survs == kaplan2(ot).survs
-@assert kaplan(ot).sd == kaplan2(ot).sd
-@assert nelson2(ot).survs == nelson2(ot).survs
-@assert nelson(ot).sd == nelson2(ot).sd
-
-m1 = median(@benchmark kaplan(ot));
-m2 = median(@benchmark );
-judge(m1, m2)
-
-km =
-
-@assert k1.survs == k2.survs
-
-km.survival
-
-confint(km)
-confint.(Ref(k2), uniqueEventTimes(ot))
-
-@btime k1 = kaplan(ot);
-@btime k2 = kaplan2(ot);
-
-n1 = @btime nelson(ot);
-n2 = @btime nelson2(ot);
-@assert n1.survs == n2.survs
-@assert n1.sd == n2.sd
-n1.sd
-
-m1 = median(@benchmark kaplan($ot))
-m2 = median(@benchmark kaplan2($ot))
-
-m3 = median(@benchmark fit(Survival.KaplanMeier, $et))
-judge(m1, m3)
-judge(m2, m3)
-
-@btime m1
-@btime m2
-@btime m3
+@benchmark confint.(Ref(nelson(ot)), uniqueEventTimes(ot))
+@benchmark confint(fit(Survival.NelsonAalen, et))
