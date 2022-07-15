97890e3cf6c426234ea320ec003f51911366e216
RaphaelS1 <raphaelsonabend@gmail.com>
2022-07-15 19:31:42 +0100

    update tests

--- src/test.jl
+++ src/test.jl
@@ -1,27 +1,28 @@
-include("src/Surv.jl")
-
-using Random: seed!
+using StatsBase
+using RecipesBase
 using BenchmarkTools
+
+using Distributions
+
+include("src/utils.jl")
+include("src/Surv.jl")
+include("src/NonParametricEstimator.jl")
+include("src/KaplanMeier.jl")
+include("src/NelsonAalen.jl")
+
 using Survival
 
-seed!(1)
 n = 1000
 T = round.(rand(Uniform(1, 10), n));
 Δ = rand(Binomial(), n) .== 1;
-# ot = Surv(T, Δ, "right");
-# Surv(T, Δ .== 1, "right")
 et = EventTime.(T, Δ);
-ot = rcSurv2(T, Δ);
+srv = Surv(T, Δ, "right");
 
 @btime EventTime.(T, Δ);
-@btime rcSurv2(T, Δ); ## longer as expected due to postprocessing
+@btime Surv(T, Δ, "right"); ## longer as expected due to postprocessing
 
-@btime Ref(kaplan2(ot)), uniqueEventTimes(ot);
-@btime fit(Survival.KaplanMeier, et);
+@btime (k = kaplan(srv); confint(k));
+@btime (k = fit(Survival.KaplanMeier, et); confint(k));
 
-
-
-
-Fₓ  = 1 .- [1, km.survs...]
-pₓ = [Fₓ[1], diff(Fₓ)...]
-d = DiscreteNonParametric([0, km.time...],  pₓ, check_args = false)
+using Plots
+plot(kaplan(srv))
--- src/test_r.jl
+++ src/test_r.jl
@@ -1,24 +1,33 @@
-import Survival
 using RCall
 using Random: seed!
 using Distributions
+using BenchmarkTools
+using Survival
+using StatsBase
 seed!(1)
 n = 1000
 T = round.(rand(Uniform(1, 10), n));
 Δ = rand(Binomial(), n) .== 1;
 
+surv = Surv(T, Δ, "right");
 
 R"
     library(survival)
     time = $T
     status = $Δ
     surv = Surv(time, status)
+";
+
+@benchmark R"
     km = survfit(surv ~ 1)
+"
+
+@benchmark kaplan(surv)
+
+R"
     paste(round(km$lower, 2),
     round(km$upper, 2), sep = ',')
 "
 
-surv = Survival.Surv(T, Δ, "right");
-km = Survival.kaplan(surv);
-km.survival
-Survival.confint(km)
+km = kaplan(surv);
+confint(km)
