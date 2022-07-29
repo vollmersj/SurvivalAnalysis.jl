0f9d06ac246b43ec4f4992453ab0d74d7544085d
RaphaelS1 <raphaelsonabend@gmail.com>
2022-07-29 17:53:18 +0100

    fix plots

--- src/plot_survivalestimator.jl
+++ src/plot_survivalestimator.jl
@@ -1,7 +1,5 @@
-@recipe function f(
-    npe::SurvivalEstimator, plot_confint::Bool = true;
-    α::Float64 = 0.05
-)
+@recipe function f(npe::SurvivalEstimator, plot_confint::Bool = true; lvl = 0.05)
+    (lvl >= 0 && lvl <= 1) || throw(ArgumentError("α must be a number in [0, 1]"))
     seriestype := :steppost
     ylims := (0, 1)
     legend := false
@@ -11,7 +9,7 @@
     end
     if plot_confint
         linecolor   --> :blue
-        cis = confint.(Ref(npe), npe.time, α = α)
+        cis = confint.(Ref(npe), npe.time, α = lvl)
         lb = map(x -> x[1], cis)
         ub = map(x -> x[2], cis)
         @series begin
