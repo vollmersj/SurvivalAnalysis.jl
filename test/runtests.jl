2b76917801e264ab747e9f5e261ec653f289a85e
RaphaelS1 <raphaelsonabend@gmail.com>
2022-09-01 19:26:21 +0100

    limit compat to julia 1.2+

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using Distributions
 using DataFrames
 using StatsModels
 
-const files = setdiff(readdir(), ["runtests.jl"])
+const files = setdiff(readdir(), ["runtests.jl", "Project.toml", "Manifest.toml"])
 
 for f in files
     @testset "$(f)" begin
