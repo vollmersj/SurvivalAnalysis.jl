f58ce6e52f08dc7c047e98c9dd4a1b2f0ba61d86
RaphaelS1 <raphaelsonabend@gmail.com>
2022-09-02 08:08:18 +0100

    update doc coverage flow

--- docs/check_doc_coverage.jl
+++ docs/check_doc_coverage.jl
@@ -11,10 +11,12 @@ exp = f[(findfirst("# documented", f)[end]+1):(findlast("# documented", f)[1]-1)
 exp = filter(x -> x != "", strip.(split(exp, r"export|\n|,")));
 
 ed_diff = setdiff(exp, documented);
-de_diff = setdiff(documented, exp)
+de_diff = setdiff(documented, exp);
 
 if length(ed_diff) > 0
-    @error "Forgot to document:" ed_diff
+    e = "Forgot to document: $(ed_diff)"
+    @error e
+    error(e)
 elseif length(de_diff) > 0
     @warn "Extra objects documented:" de_diff
 else
